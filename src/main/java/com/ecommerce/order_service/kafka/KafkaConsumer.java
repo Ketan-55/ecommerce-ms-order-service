2dec08e1194fdf9c6938b86c10fbf4d7e9e241af
Ketan Behere <Ketanbehere555@gmail.com>
2026-05-03 13:27:22 +0530

    feat: added Kafka retry mechanism with backoff for order event processing

--- src/main/java/com/ecommerce/order_service/kafka/KafkaConsumer.java
+++ src/main/java/com/ecommerce/order_service/kafka/KafkaConsumer.java
@@ -23,17 +23,16 @@ public class KafkaConsumer {
 
         String url ="http://localhost:8084/products/"+ event.getProductId()+"/reduce-stock?quantity="+ event.getQuantity();
 
-        try{
+
             webClient.put().
                     uri(url).
                     retrieve().
                     bodyToMono(String.class).
                     block();
+
             System.out.println("✅ Stock reduced for product: " + event.getProductId());
-        }catch (Exception e){
-            System.out.println("❌ Failed to reduce stock for product: " + event.getProductId() + " - " + e.getMessage());
-            e.printStackTrace();
+
 
         }
-    }
 }
+
---
+++ src/main/java/com/ecommerce/order_service/kafka/KafkaConsumerConfig.java
@@ -0,0 +1,23 @@
+package com.ecommerce.order_service.kafka;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String,Object>kafkaListenerContainerFactory(ConsumerFactory<String,Object> consumerFactory){
+        ConcurrentKafkaListenerContainerFactory<String,Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+
+        //Enable Retry
+        factory.setCommonErrorHandler(new DefaultErrorHandler(new FixedBackOff(2000L,3)));
+
+        return factory;
+    }
+}
